20e92c2b8f66027afba2e9d60687a91b1097c154
Onkar <orkawathe2003@gmail.com>
2024-10-09 23:47:48 +0530

    solved bug in userStatusrepo

--- src/main/java/com/example/chat_management/repository/UserStatusRepository.java
+++ src/main/java/com/example/chat_management/repository/UserStatusRepository.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 
+
 @Repository
-public interface UserStatusRepository extends JpaRepository<UserStatus, Long> {
+public interface UserStatusRepository extends JpaRepository<UserStatus, String> {
     Optional<UserStatus> findByUserNumber(String userNumber);
 }
